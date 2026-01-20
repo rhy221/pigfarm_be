d96ccd6ab9725d03527d879f85df633413e43836
rhy221 <ghuy9366@gmail.com>
2026-01-21 01:19:14 +0700

    fix: fix bug

--- src/feeding/feeding.module.ts
+++ src/feeding/feeding.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FeedingService } from './feeding.service';
-import { FeedingController } from './feeding.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [FeedingController],
   providers: [FeedingService]
 })
 export class FeedingModule {}
