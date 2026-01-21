c1f5931b6ac08b0a29889e9046597ffabfc65502
Hoang-hai-yen <23521847@gm.uit.edu.vn>
2026-01-21 14:34:11 +0700

    fix feeding

--- src/feeding/feeding.module.ts
+++ src/feeding/feeding.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FeedingService } from './feeding.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { FeedingController } from './feeding.controller'; 
 
 @Module({
   imports: [PrismaModule],
+  controllers: [FeedingController], 
   providers: [FeedingService],
 })
 export class FeedingModule {}
